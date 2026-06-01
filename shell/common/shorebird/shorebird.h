7d127c49b2e071b73c3b984e3b8ee146a17f2996
Patchwing <patchwing@local>
2026-06-01 15:31:10 +0800

    patchwing: simplify shorebird.h to stub (remove embedder.h include)

--- shell/common/shorebird/shorebird.h
+++ shell/common/shorebird/shorebird.h
@@ -2,40 +2,12 @@
 #define FLUTTER_SHELL_COMMON_SHOREBIRD_SHOREBIRD_H_
 
 #include "flutter/common/settings.h"
-#include "shell/platform/embedder/embedder.h"
 
 namespace flutter {
 
-struct ReleaseVersion {
-  std::string version;
-  std::string build_number;
-};
-
-struct ShorebirdConfigArgs {
-  std::string code_cache_path;
-  std::string app_storage_path;
-  std::string release_app_library_path;
-  std::string shorebird_yaml;
-  ReleaseVersion release_version;
-
-  ShorebirdConfigArgs(std::string code_cache_path,
-                      std::string app_storage_path,
-                      std::string release_app_library_path,
-                      std::string shorebird_yaml,
-                      ReleaseVersion release_version)
-      : code_cache_path(code_cache_path),
-        app_storage_path(app_storage_path),
-        release_app_library_path(release_app_library_path),
-        shorebird_yaml(shorebird_yaml),
-        release_version(release_version) {}
-};
-
-bool ConfigureShorebird(const ShorebirdConfigArgs& args,
-                        std::string& patch_path);
-
 void ConfigureShorebird(std::string code_cache_path,
                         std::string app_storage_path,
-                        Settings& settings,
+                        flutter::Settings& settings,
                         const std::string& shorebird_yaml,
                         const std::string& version,
                         const std::string& version_code);
